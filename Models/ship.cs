9b0433d84d2e21a572de0f21b1cf5e8df28d59c9
meghan munford <meghan@munford.dev>
2025-03-03 09:15:05 -0600

    added ship model

--- Models/ship.cs
+++ Models/ship.cs
@@ -4,11 +4,14 @@ namespace ShipCreator1.Models;
 
 public class ship
 {
-    [Required]
-    public int ShipID { get; set; }
     
+    public int ShipID { get; set; }
+    [Required]
     public string ShipName { get; set; }
+    [Required]
     public string ShipType { get; set; }
+    [Range(0, int.MaxValue)]
     public int NauticalMilage { get; set; }
+    public string PledgedFaction { get; set; }
     
 }
